2f15fb8cfa213c72aad0026ebc8a6fd926c00dd4
AlexanderGhosty <GOblin200>
2023-03-10 11:17:32 +0300

    New ver.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -18,9 +18,7 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         string DecimalSeparator => CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator;
@@ -47,6 +45,7 @@ namespace Calculator
             {
                 if (value2 == 0)
                 {
+                    MessageBox.Show("Ошибка: Попытка деления на 0", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                     return 0;
                 }
                 return value1 / value2;
@@ -61,6 +60,8 @@ namespace Calculator
         Operation CurrOperation;
         decimal FirstValue { get; set; }
         string oper = "";
+        decimal buffer = 0;
+        bool historyWritingFlag = true;
 
         public MainWindow()
         {
@@ -70,7 +71,9 @@ namespace Calculator
             btnAddition.Tag = new Addition();
             btnMultiplication.Tag = new Multiplication();
             btnDivision.Tag = new Division();
-            btnSubtraction.Tag = new Substraction();    
+            btnSubtraction.Tag = new Substraction();
+            RadioButtonOn.IsChecked = true;
+            historyWritingFlag = true;
         }
 
         private void regButtonClick(object sender, RoutedEventArgs e)
@@ -91,10 +94,16 @@ namespace Calculator
                 return;
             if (TextInput.Text.Length == 1 || TextInput.Text.Length == 0)
                 TextInput.Text = "0";
+            else if (TempTextInput.Text == "")
+                TextInput.Text = TextInput.Text.Substring(0, TextInput.Text.Length - 1);
+            else if (CurrOperation == null)
+            {
+                TempTextInput.Text = "";
+            }
             else
                 TextInput.Text = TextInput.Text.Substring(0, TextInput.Text.Length - 1);
             
-            FirstValue = Convert.ToDecimal(TextInput.Text);
+            //FirstValue = Convert.ToDecimal(TextInput.Text);
             if (TextInput.Text == "")
             {
                 TextInput.Text = "0";
@@ -116,11 +125,27 @@ namespace Calculator
         private void btnOperation(object sender, RoutedEventArgs e)
         {
             if (CurrOperation == null)
+            {
                 FirstValue = Convert.ToDecimal(TextInput.Text);
-            CurrOperation = (Operation)((Button)sender).Tag;
+                buffer = FirstValue;
+            }
+            if (CurrOperation != null && TextInput.Text != "")
+            {
+                if(historyWritingFlag == true)
+                {
+                    IsEmptyHistoryBox.Text = "";
+                    HistoryBox.Text += Math.Round(FirstValue, 9).ToString() + " " + CurrOperation.GetOperation();
+                }
+                    
+                FirstValue = CurrOperation.DoOperation(FirstValue, Convert.ToDecimal(TextInput.Text));
+                if (historyWritingFlag == true)
+                    HistoryBox.Text += " " + TextInput.Text + " = " + Math.Round(FirstValue, 9).ToString() + "\n";
+            }
 
-            TempTextInput.Text = TextInput.Text + CurrOperation.GetOperation();
+            CurrOperation = (Operation)((Button)sender).Tag;
+            TempTextInput.Text = Math.Round(FirstValue, 9) + CurrOperation.GetOperation();
             TextInput.Text = "";
+            
 
         }
         private void btnAllClear_Click(object sender, RoutedEventArgs e)
@@ -139,9 +164,72 @@ namespace Calculator
             decimal value2 = Convert.ToDecimal(TextInput.Text);
             TempTextInput.Text += value2;
             TempTextInput.Text += "=";
-            TextInput.Text = CurrOperation.DoOperation(FirstValue, value2).ToString();
-            FirstValue = Convert.ToDecimal(TextInput.Text);
+
+            if (historyWritingFlag == true)
+                HistoryBox.Text += FirstValue.ToString() + " " + CurrOperation.GetOperation() + " " + value2 + " = ";
+            FirstValue = CurrOperation.DoOperation(FirstValue, value2);
+            TextInput.Text = Math.Round(FirstValue, 9).ToString();
+
+            if (historyWritingFlag == true)
+                HistoryBox.Text += Math.Round(FirstValue, 9).ToString() + "\n";
             CurrOperation = null;
+        }
+
+        private void historyButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (historyGrid.Visibility == Visibility.Visible)
+                historyGrid.Visibility = Visibility.Collapsed;
+            else
+                historyGrid.Visibility = Visibility.Visible;
+        }
+
+        private void historyClearButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (HistoryBox.Text != "")
+            {
+                HistoryBox.Text = "";
+                IsEmptyHistoryBox.Text = "Журнал операций пуст";
+            }
+        }
+        private void historyBackButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (HistoryBox.Text != "")
+            {
+                string str = HistoryBox.Text;
+                int lastSlashIndex = str.LastIndexOf('\n');
+                if (lastSlashIndex >= 0) // Проверяем, что символ '/' найден
+                {
+                    str = str.Substring(0, lastSlashIndex); // Получаем подстроку после последнего символа '/'
+                    lastSlashIndex = str.LastIndexOf('\n');
+                    str = str.Substring(0, lastSlashIndex + 1);
+                }
+                else
+                {
+                    str = "";
+                }
+                HistoryBox.Text = str;
+                if (HistoryBox.Text == "")
+                    IsEmptyHistoryBox.Text = "Журнал операций пуст";
+            }
+        }
+        private void RadioButtonOff_Checked(object sender, RoutedEventArgs e) 
+        {
+            historyButton.Visibility = Visibility.Collapsed;
+            writeHistoryButton.Visibility = Visibility.Collapsed;
+        }
+        private void RadioButtonOn_Checked(object sender, RoutedEventArgs e)
+        {
+            historyButton.Visibility = Visibility.Visible;
+            writeHistoryButton.Visibility = Visibility.Visible;
+        }
+
+        private void writeHistoryButton_Checked(object sender, RoutedEventArgs e)
+        {
+            historyWritingFlag = true;
+        }
+        private void writeHistoryButton_UnChecked(object sender, RoutedEventArgs e)
+        {
+            historyWritingFlag = false;
         }
     }
 }
